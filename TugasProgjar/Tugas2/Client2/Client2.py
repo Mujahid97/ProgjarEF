bcca25c7d478283df1943797588737737679aa9e
Mujahid97 <33047680+Mujahid97@users.noreply.github.com>
2019-03-26 23:11:01 +0700

    Final

--- TugasProgjar/Tugas2/Client2/Client2.py
+++ TugasProgjar/Tugas2/Client2/Client2.py
@@ -11,7 +11,7 @@ num = int
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((UDP_IP, UDP_PORT))
 waktu = datetime.datetime.now()
-with open('client_received_file'+ format(waktu) + '.jpeg', 'wb') as f:
+with open('client2_received_file'+ format(waktu) + '.jpeg', 'wb') as f:
     print 'file opened'
     while True:
         data = s.recv(BUFFER_SIZE)
---
+++ TugasProgjar/Tugas2/run.sh
@@ -0,0 +1,14 @@
+cd Client1
+python Client1.py
+
+cd ../Client2
+python Client2.py
+
+cd ../Client3
+python Client3.py
+
+cd ../Client4
+python Client4.py
+
+cd ..
+python
